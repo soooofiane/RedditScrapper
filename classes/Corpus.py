c56e3f8dcc86e8c078d1753b7975961152aa6369
Sofiane Tadimi <stadimi@univ-lyon2.fr>
2025-10-23 16:00:02 +0200

    class update

---
+++ classes/Corpus.py
@@ -0,0 +1,12 @@
+class Corpus:
+    def __init__(self, nom, authors, id2doc, ndoc, naut):
+        self.nom = nom
+        self authors = authors
+        self.id2doc = id2doc
+        self.ndoc = ndoc
+        self.naut = naut
+
+    def sort_title(self, num):
+        
+
+
